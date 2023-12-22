5a5e3195d8dfbf813fde9bdbc986fda681e27cb3
AnAppleCore <994164148@qq.com>
2023-12-23 07:15:47 +0800

    composing v2

--- src/components/wish-item-chagpt.jsx
+++ src/components/wish-item-chagpt.jsx
@@ -8,28 +8,64 @@ export default function WishItemChagpt(props) {
   const {src, id, assignedRating, teaName, assignedLevelChs, color} = props.item
   return (
     <Col
-    xs="2"
-    md="2"
-    lg="2"
-    style={{
-      backgroundImage: `url('${TeaTypeImages('./' + src).default}')`,
-      backgroundPositionX: itemPercentX+'%',
-      height: '600px',
-    }}
-    className={`wish-item character mx-1 px-0`}>
-      <div
-      className="h-100 react-stars-container d-flex flex-column align-content-center justify-content-end pb-2">
-        <div className="text-center text-wrap pb-1" style={{ fontSize: '4.0rem' }}>{teaName}</div>
-        <div className="text-center text-wrap pb-1" style={{ fontSize: '5.0rem' }}>{id}</div>
-        <div className="text-center text-wrap pb-1" style={{ fontSize: '4.0rem', 'color': color}}>{assignedLevelChs}</div>
-        {/* <ReactStars
-          count={assignedRating}
-          size={80}
-          edit={false}
-          color="#ffd700"
-          classNames={"justify-content-center"}
-        /> */}
-      </div>
+      xs="2"
+      md="2"
+      lg="2"
+      className={`wish-item character mx-1 px-0`}
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        height: '600px',
+      }}>
+
+      <div className="text-center text-wrap" style={{ fontSize: '3.0rem', color: color }}>{assignedLevelChs}</div>
+
+      <div style={{
+        backgroundImage: `url('${TeaTypeImages('./' + src).default}')`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        flex: '1',
+      }}></div>
+      <div className="text-center text-wrap" style={{ fontSize: '3.0rem'}}>{teaName}</div>
+      <div className="text-center text-wrap" style={{ fontSize: '3.0rem', letterSpacing: '15px'}}>{integerToChineseString(id)}</div>
     </Col>
+
+    // <Col
+    // xs="2"
+    // md="2"
+    // lg="2"
+    // style={{
+    //   backgroundImage: `url('${TeaTypeImages('./' + src).default}')`,
+    //   backgroundPositionX: itemPercentX+'%',
+    //   height: '600px',
+    // }}
+    // className={`wish-item character mx-1 px-0`}>
+    //   <div
+    //   className="h-100 react-stars-container d-flex flex-column align-content-center justify-content-end pb-2">
+    //     <div className="text-center text-wrap pb-1" style={{ fontSize: '3.0rem' }}>{teaName}</div>
+    //     <div className="text-center text-wrap pb-1" style={{ fontSize: '3.0rem' }}>{integerToChineseString(id)}</div>
+    //     <div className="text-center text-wrap pb-1" style={{ fontSize: '3.0rem', 'color': color}}>{assignedLevelChs}</div>
+    //     {/* <ReactStars
+    //       count={assignedRating}
+    //       size={80}
+    //       edit={false}
+    //       color="#ffd700"
+    //       classNames={"justify-content-center"}
+    //     /> */}
+    //   </div>
+    // </Col>
   )
+}
+
+function integerToChineseString(num) {
+  const chineseDigits = ['〇', '一', '二', '三', '四', '五', '六', '七', '八', '九'];
+  let chineseString = num.toString().split('').map(digit => chineseDigits[parseInt(digit)]).join('');
+
+  // Pad with '〇' to make the string 3 characters long
+  while (chineseString.length < 3) {
+      chineseString = '〇' + chineseString;
+  }
+
+  return chineseString;
 }
